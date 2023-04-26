24eebfdc763e699390edcbb83a183424aeec8347
Venkataravali <ravalinekkanti@gmail.com>
2023-04-26 22:12:58 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import Adminapp from './admin/adminapp';
+import Userapp from './user/userapp';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ if (localStorage.getItem("sellerid") == null)
+{
 
+return ( <Userapp />);
+}
+else{
+  return ( <Adminapp/>);
+
+}
+}
 export default App;
